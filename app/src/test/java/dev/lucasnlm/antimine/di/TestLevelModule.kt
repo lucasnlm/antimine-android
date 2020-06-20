39f58907cf4ff191d7a7ffa99cf39b22d6939c2c
Lucas Lima <me@lucasnlm.dev>
2020-06-20 16:04:11 -0300

    Fix ktlint

--- app/src/test/java/dev/lucasnlm/antimine/di/TestLevelModule.kt
+++ app/src/test/java/dev/lucasnlm/antimine/di/TestLevelModule.kt
@@ -1,12 +1,10 @@
 package dev.lucasnlm.antimine.di
 
-import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.lucasnlm.antimine.common.level.models.Event
 import dev.lucasnlm.antimine.common.level.repository.IDimensionRepository
 import dev.lucasnlm.antimine.common.level.repository.IMinefieldRepository
@@ -16,8 +14,6 @@ import dev.lucasnlm.antimine.common.level.repository.MemorySavesRepository
 import dev.lucasnlm.antimine.common.level.repository.MemoryStatsRepository
 import dev.lucasnlm.antimine.common.level.utils.Clock
 import dev.lucasnlm.antimine.common.level.utils.IHapticFeedbackInteractor
-import dev.lucasnlm.antimine.core.analytics.AnalyticsManager
-import dev.lucasnlm.antimine.core.preferences.IPreferencesRepository
 import dev.lucasnlm.antimine.mocks.DisabledHapticFeedbackInteractor
 import dev.lucasnlm.antimine.mocks.FixedDimensionRepository
 import dev.lucasnlm.antimine.mocks.FixedMinefieldRepository
--- common/src/main/java/dev/lucasnlm/antimine/common/level/view/CommonLevelFragment.kt
+++ common/src/main/java/dev/lucasnlm/antimine/common/level/view/CommonLevelFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import dev.lucasnlm.antimine.common.level.repository.IDimensionRepository
 import dev.lucasnlm.antimine.common.level.viewmodel.GameViewModel
