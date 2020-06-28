5c46f73328d0054ad753ee2090e6c6157614857f
Lucas Lima <lucas.lima@nubank.com.br>
2020-06-28 01:03:02 -0300

    Simplify Fragments

--- app/src/main/java/dev/lucasnlm/antimine/about/views/AboutInfoFragment.kt
+++ app/src/main/java/dev/lucasnlm/antimine/about/views/AboutInfoFragment.kt
@@ -1,33 +1,17 @@
 package dev.lucasnlm.antimine.about.views
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.activityViewModels
 import dev.lucasnlm.antimine.BuildConfig
 import dev.lucasnlm.antimine.R
 import dev.lucasnlm.antimine.about.models.AboutEvent
 import dev.lucasnlm.antimine.about.viewmodel.AboutViewModel
 import kotlinx.android.synthetic.main.fragment_about_info.*
 
-class AboutInfoFragment : Fragment() {
-    private var aboutViewModel: AboutViewModel? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.run {
-            aboutViewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_about_info, container, false)
+class AboutInfoFragment : Fragment(R.layout.fragment_about_info) {
+    private val aboutViewModel: AboutViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,15 +19,15 @@ class AboutInfoFragment : Fragment() {
         version.text = getString(R.string.version_s, BuildConfig.VERSION_NAME)
 
         thirdsParties.setOnClickListener {
-            aboutViewModel?.eventObserver?.postValue(AboutEvent.ThirdPartyLicenses)
+            aboutViewModel.eventObserver.postValue(AboutEvent.ThirdPartyLicenses)
         }
 
         sourceCode.setOnClickListener {
-            aboutViewModel?.eventObserver?.postValue(AboutEvent.SourceCode)
+            aboutViewModel.eventObserver.postValue(AboutEvent.SourceCode)
         }
 
         translation.setOnClickListener {
-            aboutViewModel?.eventObserver?.postValue(AboutEvent.Translators)
+            aboutViewModel.eventObserver.postValue(AboutEvent.Translators)
         }
     }
 }
--- app/src/main/java/dev/lucasnlm/antimine/about/views/thirds/ThirdPartiesFragment.kt
+++ app/src/main/java/dev/lucasnlm/antimine/about/views/thirds/ThirdPartiesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,27 +14,13 @@ import dev.lucasnlm.antimine.R
 import dev.lucasnlm.antimine.about.viewmodel.AboutViewModel
 import kotlinx.android.synthetic.main.fragment_third_party.*
 
-class ThirdPartiesFragment : Fragment() {
-    private var aboutViewModel: AboutViewModel? = null
+class ThirdPartiesFragment : Fragment(R.layout.fragment_third_party) {
+    private val aboutViewModel: AboutViewModel by activityViewModels()
 
     override fun onResume() {
         super.onResume()
         activity?.title = getString(R.string.licenses)
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.run {
-            aboutViewModel = ViewModelProviders.of(this).get(AboutViewModel::class.java)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_third_party, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -46,7 +33,7 @@ class ThirdPartiesFragment : Fragment() {
             layoutManager = LinearLayoutManager(context)
         }
 
-        licenses.adapter = aboutViewModel?.getLicenses()
+        licenses.adapter = aboutViewModel.getLicenses()
     }
 
     companion object {
