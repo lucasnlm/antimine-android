a1500cadc7488a6ce1e07cece99c98728760836f
Lucas Lima <me@lucasnlm.dev>
2020-07-04 11:47:06 -0300

    Simplify MinefieldCreator

--- common/src/main/java/dev/lucasnlm/antimine/common/level/logic/MinefieldCreator.kt
+++ common/src/main/java/dev/lucasnlm/antimine/common/level/logic/MinefieldCreator.kt
@@ -2,7 +2,6 @@ package dev.lucasnlm.antimine.common.level.logic
 
 import dev.lucasnlm.antimine.common.level.models.Area
 import dev.lucasnlm.antimine.common.level.models.Minefield
-import kotlin.math.absoluteValue
 import kotlin.math.floor
 import kotlin.random.Random
 
@@ -10,15 +9,7 @@ class MinefieldCreator(
     private val minefield: Minefield,
     private val randomGenerator: Random
 ) {
-    private class MapItem(
-        val id: Int,
-        val posX: Int,
-        val posY: Int,
-        var minesAround: Int = 0,
-        var hasMine: Boolean = false
-    )
-
-    private fun createEmptyField(): List<MapItem> {
+    private fun createEmptyField(): MutableList<Area> {
         val width = minefield.width
         val height = minefield.height
         val fieldSize = width * height
@@ -26,48 +17,31 @@ class MinefieldCreator(
         return (0 until fieldSize).map { index ->
             val yPosition = floor((index / width).toDouble()).toInt()
             val xPosition = (index % width)
-            MapItem(
+            Area(
                 index,
                 xPosition,
-                yPosition
+                yPosition,
+                0,
+                safeZone = false,
+                hasMine = false
             )
-        }.toList()
+        }.toMutableList()
     }
 
     fun createMap(safeIndex: Int): List<Area> {
-        val minefieldMap = createEmptyField()
-        val safeArea = minefieldMap.getArea(safeIndex)
-
-        // Plant mines and setup number tips
-        minefieldMap
-            .filterNot {
-                ((it.posX - safeArea.posX).absoluteValue <= 1 && (it.posY - safeArea.posY).absoluteValue <= 1)
-            }
-            .toSet()
-            .shuffled(randomGenerator)
-            .take(minefield.mines)
-            .onEach {
-                it.hasMine = true
-                it.findNeighbors(minefieldMap).forEach { neighbor ->
-                    neighbor.minesAround = neighbor.minesAround + 1
+        return createEmptyField().apply {
+            // Plant mines and setup number tips
+            filterNotNeighborsOf(safeIndex)
+                .shuffled(randomGenerator)
+                .take(minefield.mines)
+                .onEach {
+                    filterNeighborsOf(it).forEach { neighbor ->
+                        this[neighbor.id] = this[neighbor.id].copy(minesAround = neighbor.minesAround + 1)
+                    }
                 }
-            }
-            .onEach {
-                it.minesAround = 0
-            }
-
-        return minefieldMap.map {
-            Area(it.id, it.posX, it.posY, it.minesAround, false, it.hasMine)
-        }.toList()
-    }
-
-    private fun List<MapItem>.getArea(id: Int) = this.first { it.id == id }
-
-    private fun MapItem.findNeighbors(onMap: List<MapItem>) = sequenceOf(
-        1 to 0, 1 to 1, 0 to 1, -1 to 1, -1 to 0, -1 to -1, 0 to -1, 1 to -1
-    ).map { (x, y) -> getNeighbor(x, y, onMap) }.filterNotNull()
-
-    private fun MapItem.getNeighbor(x: Int, y: Int, onMap: List<MapItem>) = onMap.firstOrNull {
-        (it.posX == this.posX + x) && (it.posY == this.posY + y)
+                .onEach {
+                    this[it.id] = this[it.id].copy(hasMine = true, minesAround = 0)
+                }
+        }
     }
 }
