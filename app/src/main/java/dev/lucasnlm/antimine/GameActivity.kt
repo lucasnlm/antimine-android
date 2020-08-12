032b50fdbfdc79d0ed323cf819d881f2e54cd237
Lucas Lima <lucas.lima@nubank.com.br>
2020-08-11 23:35:46 -0300

    Fix ktlint

--- app/src/main/java/dev/lucasnlm/antimine/GameActivity.kt
+++ app/src/main/java/dev/lucasnlm/antimine/GameActivity.kt
@@ -3,7 +3,6 @@ package dev.lucasnlm.antimine
 import android.content.ActivityNotFoundException
 import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
