f5ed7d9416ffa36e4ed952f130d302c9fcd8c504
Lucas Lima <me@lucasnlm.dev>
2020-08-31 20:05:05 -0300

    Fix crash

--- common/src/main/java/dev/lucasnlm/antimine/common/level/viewmodel/GameViewModel.kt
+++ common/src/main/java/dev/lucasnlm/antimine/common/level/viewmodel/GameViewModel.kt
@@ -216,7 +216,7 @@ class GameViewModel(
     }
 
     suspend fun saveGame() {
-        if (gameController.hasMines()) {
+        if (initialized && gameController.hasMines()) {
             val id = savesRepository.saveGame(
                 gameController.getSaveState(elapsedTimeSeconds.value ?: 0L, currentDifficulty)
             )
