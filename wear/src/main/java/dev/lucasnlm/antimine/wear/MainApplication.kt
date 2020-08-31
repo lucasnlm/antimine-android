62a803d91e598df73e61d1f8a5b0a96a516138b8
Lucas Lima <lucas.lima@nubank.com.br>
2020-08-30 22:26:10 -0300

    Fix wear build

--- wear/src/main/java/dev/lucasnlm/antimine/wear/MainApplication.kt
+++ wear/src/main/java/dev/lucasnlm/antimine/wear/MainApplication.kt
@@ -1,5 +1,29 @@
 package dev.lucasnlm.antimine.wear
 
 import android.app.Application
+import dev.lucasnlm.antimine.common.level.di.LevelModule
+import dev.lucasnlm.antimine.core.analytics.IAnalyticsManager
+import dev.lucasnlm.antimine.core.analytics.models.Analytics
+import dev.lucasnlm.antimine.core.di.CommonModule
+import dev.lucasnlm.antimine.wear.di.AppModule
+import dev.lucasnlm.antimine.wear.di.ViewModelModule
+import org.koin.android.ext.android.inject
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class MainApplication : Application()
+open class MainApplication : Application() {
+    private val analyticsManager: IAnalyticsManager by inject()
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(applicationContext)
+            modules(AppModule, CommonModule, LevelModule, ViewModelModule)
+        }
+
+        analyticsManager.apply {
+            setup(applicationContext, mapOf())
+            sentEvent(Analytics.Open)
+        }
+    }
+}
