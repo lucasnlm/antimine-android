c9b5f4bebf114a12b06d53377e355490bad7b1a4
OsirisInferi <github@flut.demon.nl>
2020-12-27 22:05:23 +0100

    Average time calculated with just the victories

--- app/src/main/java/dev/lucasnlm/antimine/stats/model/StatsModel.kt
+++ app/src/main/java/dev/lucasnlm/antimine/stats/model/StatsModel.kt
@@ -6,6 +6,7 @@ data class StatsModel(
     @StringRes val title: Int,
     val totalGames: Int,
     val totalTime: Long,
+    val victoryTime: Long,
     val averageTime: Long,
     val shortestTime: Long,
     val mines: Int,
