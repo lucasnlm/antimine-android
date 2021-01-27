76d7de26a0dc5f429629ab81d6eb6a66cc4aba7b
Lucas Lima <lucasnunesk@gmail.com>
2021-01-27 20:13:13 -0300

    Fix build

--- foss/src/main/java/dev/lucasnlm/external/AdsManager.kt
+++ foss/src/main/java/dev/lucasnlm/external/AdsManager.kt
@@ -3,7 +3,9 @@ package dev.lucasnlm.external
 import android.app.Activity
 import android.content.Context
 
-class AdsManager : IAdsManager {
+class AdsManager(
+    private val crashReporter: ICrashReporter,
+) : IAdsManager {
     override fun start(context: Context) { }
 
     override fun requestRewardedAd(
