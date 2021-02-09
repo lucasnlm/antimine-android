bfa556b0b2a76a64bcc506c78b05dc1795d228e0
Lucas Lima <lucasnunesk@gmail.com>
2021-02-09 20:25:35 -0300

    Fix tests

--- wear/src/main/java/dev/lucasnlm/antimine/wear/di/AppModule.kt
+++ wear/src/main/java/dev/lucasnlm/antimine/wear/di/AppModule.kt
@@ -7,10 +7,12 @@ import dev.lucasnlm.external.GoogleAdsManager
 import dev.lucasnlm.external.BillingManager
 import dev.lucasnlm.external.CrashReporter
 import dev.lucasnlm.external.ExternalAnalyticsWrapper
+import dev.lucasnlm.external.FeatureFlagManager
 import dev.lucasnlm.external.IAdsManager
 import dev.lucasnlm.external.IAnalyticsManager
 import dev.lucasnlm.external.IBillingManager
 import dev.lucasnlm.external.ICrashReporter
+import dev.lucasnlm.external.IFeatureFlagManager
 import dev.lucasnlm.external.IInstantAppManager
 import dev.lucasnlm.external.IPlayGamesManager
 import dev.lucasnlm.external.IReviewWrapper
@@ -32,6 +34,8 @@ val AppModule = module {
     single { ReviewWrapper() } bind IReviewWrapper::class
 
     single { CrashReporter() } bind ICrashReporter::class
+
+    single { FeatureFlagManager() } bind IFeatureFlagManager::class
 
     single {
         if (BuildConfig.DEBUG) {
