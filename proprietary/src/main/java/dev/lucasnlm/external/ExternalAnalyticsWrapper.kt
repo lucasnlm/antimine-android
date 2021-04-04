932e52161379cdc687b1c47be8a48a04fc2a1b2a
Lucas Lima <lucasnunesk@gmail.com>
2021-04-04 16:11:22 -0300

    Add amplitude

--- proprietary/src/main/java/dev/lucasnlm/external/ExternalAnalyticsWrapper.kt
+++ proprietary/src/main/java/dev/lucasnlm/external/ExternalAnalyticsWrapper.kt
@@ -2,7 +2,9 @@ package dev.lucasnlm.external
 
 import android.content.Context
 import android.os.Bundle
+import com.amplitude.api.Amplitude
 import com.google.firebase.analytics.FirebaseAnalytics
+import org.json.JSONObject
 
 class ExternalAnalyticsWrapper(
     private val context: Context,
@@ -10,11 +12,19 @@ class ExternalAnalyticsWrapper(
     private val firebaseAnalytics by lazy {
         FirebaseAnalytics.getInstance(context)
     }
+    private val amplitudeClient by lazy {
+        Amplitude
+            .getInstance()
+            .enableCoppaControl()
+            .initialize(context, "AMPLITUDE_API_KEY");
+    }
 
     override fun setup(context: Context, properties: Map<String, String>) {
         properties.forEach { (key, value) ->
             firebaseAnalytics.setUserProperty(key, value)
         }
+
+        amplitudeClient.setUserProperties(JSONObject(properties))
     }
 
     override fun sendEvent(name: String, content: Map<String, String>) {
@@ -26,5 +36,6 @@ class ExternalAnalyticsWrapper(
         }
 
         firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        amplitudeClient.logEvent(name, JSONObject(content))
     }
 }
