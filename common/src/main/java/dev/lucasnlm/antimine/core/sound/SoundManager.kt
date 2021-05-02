7b65521bc2eda0e9f343105909d0d93219a20b2b
Lucas Lima <lucasnunesk@gmail.com>
2021-05-02 10:05:18 -0300

    Fix play sound bug

--- common/src/main/java/dev/lucasnlm/antimine/core/sound/SoundManager.kt
+++ common/src/main/java/dev/lucasnlm/antimine/core/sound/SoundManager.kt
@@ -3,6 +3,7 @@ package dev.lucasnlm.antimine.core.sound
 import android.content.Context
 import android.media.MediaPlayer
 import androidx.annotation.RawRes
+import dev.lucasnlm.external.ICrashReporter
 
 interface ISoundManager {
     fun play(@RawRes soundId: Int)
@@ -10,14 +11,21 @@ interface ISoundManager {
 
 class SoundManager(
     private val context: Context,
+    private val crashReporter: ICrashReporter,
 ) : ISoundManager {
 
     override fun play(@RawRes soundId: Int) {
-        MediaPlayer.create(context, soundId).apply {
-            setVolume(0.7f, 0.7f)
-            setOnCompletionListener {
-                release()
-            }
-        }.start()
+        try {
+            MediaPlayer.create(context, soundId).apply {
+                setVolume(0.7f, 0.7f)
+                setOnCompletionListener {
+                    release()
+                }
+            }.start()
+        } catch (e: Exception) {
+            // Some Huawei phones fail to play sounds there.
+            // Adding this try catch to at lease to make they crash.
+            crashReporter.sendError("Fail to play sound.\n${e.message}")
+        }
     }
 }
