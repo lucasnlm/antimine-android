ac3f10ef2268ca5d6fe2ab09a0dc522ea463ec87
Lucas Lima <lucasnunesk@gmail.com>
2021-06-17 10:24:34 -0300

    Fix tests

--- ui/src/main/java/dev/lucasnlm/antimine/ui/repository/Themes.kt
+++ ui/src/main/java/dev/lucasnlm/antimine/ui/repository/Themes.kt
@@ -5,7 +5,7 @@ import dev.lucasnlm.antimine.ui.model.AppTheme
 import dev.lucasnlm.antimine.ui.model.AreaPalette
 
 object Themes {
-    private fun lightTheme() = AppTheme(
+    fun lightTheme() = AppTheme(
         id = 1L,
         theme = R.style.CustomLightTheme,
         palette = AreaPalette(
