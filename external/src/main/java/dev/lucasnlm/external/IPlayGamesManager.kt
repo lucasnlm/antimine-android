0d5a8e2702dabf14376f0307f80c54f64feaf2f3
Lucas Lima <lucasnunesk@gmail.com>
2021-12-25 13:51:13 -0300

    Add more games action

--- external/src/main/java/dev/lucasnlm/external/IPlayGamesManager.kt
+++ external/src/main/java/dev/lucasnlm/external/IPlayGamesManager.kt
@@ -42,4 +42,5 @@ interface IPlayGamesManager {
     fun keepRequestingLogin(status: Boolean)
     fun shouldRequestLogin(): Boolean
     fun signInToFirebase(activity: Activity)
+    fun moreGameDeeplink(): String
 }
