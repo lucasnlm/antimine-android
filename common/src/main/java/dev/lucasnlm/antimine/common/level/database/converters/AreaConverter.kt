70db752e97096def71c2370e013e4af5f2d02009
Lucas Lima <me@lucasnlm.dev>
2022-12-28 16:18:54 -0300

    Fix lint

--- common/src/main/java/dev/lucasnlm/antimine/common/level/database/converters/AreaConverter.kt
+++ common/src/main/java/dev/lucasnlm/antimine/common/level/database/converters/AreaConverter.kt
@@ -1,6 +1,5 @@
 package dev.lucasnlm.antimine.common.level.database.converters
 
-import androidx.annotation.Keep
 import androidx.room.TypeConverter
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
