a07ae6758c0ac4123b77c09a8c5a404efd2bc7be
Lucas Lima <me@lucasnlm.dev>
2023-04-01 20:39:26 -0300

    Add konfetti support - fix

--- app/src/main/java/dev/lucasnlm/antimine/GameActivity.kt
+++ app/src/main/java/dev/lucasnlm/antimine/GameActivity.kt
@@ -352,6 +352,7 @@ class GameActivity :
                             withContext(Dispatchers.Main) {
                                 showKonfettiView()
                             }
+                            gameAudioManager.pauseMusic()
                             showEndGameToast(GameResult.Victory)
                         }
                     }
