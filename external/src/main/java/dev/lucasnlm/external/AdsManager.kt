7555e9589fe2eb7e89094472cd5aaa8df8af151d
Lucas Lima <me@lucasnlm.dev>
2023-04-30 20:55:04 -0300

    Move Ads class location

--- external/src/main/java/dev/lucasnlm/external/AdsManager.kt
+++ external/src/main/java/dev/lucasnlm/external/AdsManager.kt
@@ -29,12 +29,3 @@ interface AdsManager {
         onError: (() -> Unit)? = null,
     ): View?
 }
-
-object Ads {
-    const val RewardAd = "ca-app-pub-3940256099942544/5224354917"
-    const val SecondRewardAd = "ca-app-pub-3940256099942544/5224354918"
-    const val InterstitialAd = "ca-app-pub-3940256099942544/1033173712"
-    const val SecondInterstitialAd = "ca-app-pub-3940256099942544/1033173713"
-    const val BannerAd = "ca-app-pub-3940256099942544/6300978111"
-    const val MIN_FREQUENCY = 60 * 1000L
-}
