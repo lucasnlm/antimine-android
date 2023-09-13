9ae91f004a1c6b22e49532cdf9dfc30f014d1360
Lucas Lima <me@lucasnlm.dev>
2023-09-12 22:39:07 -0300

    Bump depedencies

--- proprietary/src/main/java/dev/lucasnlm/external/AdMobAdsManager.kt
+++ proprietary/src/main/java/dev/lucasnlm/external/AdMobAdsManager.kt
@@ -3,7 +3,14 @@ package dev.lucasnlm.external
 import android.app.Activity
 import android.content.Context
 import android.view.View
-import com.google.android.gms.ads.*
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.initialization.AdapterStatus
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
