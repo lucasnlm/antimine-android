a4887a16c96f0dee6350fcabd396378e20018826
Lucas Lima <me@lucasnlm.dev>
2023-10-22 18:46:48 -0300

    Fix lint

--- app/src/main/java/dev/lucasnlm/antimine/playgames/viewmodel/PlayGamesEvent.kt
+++ app/src/main/java/dev/lucasnlm/antimine/playgames/viewmodel/PlayGamesEvent.kt
@@ -2,5 +2,6 @@ package dev.lucasnlm.antimine.playgames.viewmodel
 
 sealed class PlayGamesEvent {
     data object OpenAchievements : PlayGamesEvent()
+
     data object OpenLeaderboards : PlayGamesEvent()
 }
--- app/src/main/java/dev/lucasnlm/antimine/stats/viewmodel/StatsEvent.kt
+++ app/src/main/java/dev/lucasnlm/antimine/stats/viewmodel/StatsEvent.kt
@@ -2,5 +2,6 @@ package dev.lucasnlm.antimine.stats.viewmodel
 
 sealed class StatsEvent {
     data object LoadStats : StatsEvent()
+
     data object DeleteStats : StatsEvent()
 }
