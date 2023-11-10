f32c163522a26ca6369dd6394f8ea435ec376c0d
Lucas Lima <me@lucasnlm.dev>
2023-11-09 23:16:36 -0300

    Improve performance of Legend level creation

--- sgtatham/src/main/cpp/minefield_creator.h
+++ sgtatham/src/main/cpp/minefield_creator.h
@@ -4,6 +4,7 @@
 #include <random>
 
 std::string new_mine_layout(
+        int slice_width,
         std::size_t width,
         std::size_t height,
         std::size_t mines_amount,
@@ -14,12 +15,23 @@ std::string new_mine_layout(
 
 std::string minefield_to_string(const std::basic_string<std::size_t> &grid);
 
+std::basic_string<std::size_t> generate_safe_minefield_sliced(
+        std::size_t slice_width,
+        std::size_t width,
+        std::size_t height,
+        std::size_t mines_amount,
+        std::size_t start_x,
+        std::size_t start_y,
+        std::mt19937 &random
+);
+
 std::basic_string<std::size_t> generate_safe_minefield(
         std::size_t width,
         std::size_t height,
         std::size_t mines_amount,
         std::size_t start_x,
         std::size_t start_y,
+        bool safe_border,
         std::mt19937 &random
 );
 
@@ -29,5 +41,6 @@ std::basic_string<std::size_t> generate_random_minefield_with_safe_area(
         std::size_t mines_amount,
         std::size_t start_x,
         std::size_t start_y,
+        bool safe_border,
         std::mt19937 &random
 );
